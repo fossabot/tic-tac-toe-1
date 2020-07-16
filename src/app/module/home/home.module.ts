3f559a3b67d3e342bfcd6baddf4d5b155785cbf6
Cholewka <mdcholewka@gmail.com>
2020-07-17 00:05:45 +0200

    feat: Create home module and its routing

---
+++ src/app/module/home/home.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { HomeComponent } from './page/home/home.component';
+
+import { HomeRoutingModule } from './home.routing';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [HomeRoutingModule],
+})
+export class HomeModule {}
