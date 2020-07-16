e2d2d44b190853b89ac62a5b120cfeda2adcc0ff
Cholewka <mdcholewka@gmail.com>
2020-07-17 00:17:25 +0200

    feat: Set title in providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,7 +12,7 @@ import { NavigationBarComponent } from './layout/navigation-bar/navigation-bar.c
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
   ],
-  providers: [],
+  providers: [Title],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
