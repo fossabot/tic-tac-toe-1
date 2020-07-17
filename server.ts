7a1f17f7e9fb2c3f77551cd18ec85e6ee4a20e77
Cholewka <mdcholewka@gmail.com>
2020-07-17 19:32:26 +0200

    feat: Add basic server configuration

--- server.ts
+++ server.ts
@@ -8,16 +8,39 @@ import { AppServerModule } from './src/main.server';
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
 
+import * as mongoose from 'mongoose';
+import * as bodyParser from 'body-parser';
+import * as passport from 'passport';
+import * as bluebird from 'bluebird';
+import * as cors from 'cors';
+
+import { MONGODB_URI } from '@api/util/secrets';
+
 // The Express app is exported so that it can be used by serverless Functions.
 export function app() {
   const server = express();
   const distFolder = join(process.cwd(), 'dist/tictactoe/browser');
-  const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
+  const indexHtml = existsSync(join(distFolder, 'index.original.html'))
+    ? 'index.original.html'
+    : 'index';
+
+  // Set body parser
+  server.use(bodyParser.json());
+  server.use(bodyParser.urlencoded({ extended: true }));
+
+  // Initialize Passport
+  server.use(passport.initialize());
+
+  // Use CORS
+  server.use(cors());
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-  server.engine('html', ngExpressEngine({
-    bootstrap: AppServerModule,
-  }));
+  server.engine(
+    'html',
+    ngExpressEngine({
+      bootstrap: AppServerModule,
+    })
+  );
 
   server.set('view engine', 'html');
   server.set('views', distFolder);
@@ -25,13 +48,19 @@ export function app() {
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get('*.*', express.static(distFolder, {
-    maxAge: '1y'
-  }));
+  server.get(
+    '*.*',
+    express.static(distFolder, {
+      maxAge: '1y',
+    })
+  );
 
   // All regular routes use the Universal engine
   server.get('*', (req, res) => {
-    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
+    res.render(indexHtml, {
+      req,
+      providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }],
+    });
   });
 
   return server;
@@ -42,6 +71,26 @@ function run() {
 
   // Start up the Node server
   const server = app();
+
+  // Connect to MongoDB database
+  (<any>mongoose).Promise = bluebird;
+
+  mongoose
+    .connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    })
+    .then(() =>
+      console.info('Successfully obtained connection with MongoDB database.')
+    )
+    .catch(() => {
+      console.error(
+        `MongoDB connection error. Please make sure you have provided valid MongoDB credentials.`
+      );
+      process.exit(1);
+    });
+
   server.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
@@ -52,7 +101,7 @@ function run() {
 // The below code is to ensure that the server is run only when not requiring the bundle.
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
-const moduleFilename = mainModule && mainModule.filename || '';
+const moduleFilename = (mainModule && mainModule.filename) || '';
 if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
   run();
 }
