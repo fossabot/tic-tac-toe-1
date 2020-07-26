9a6cf80280b78ffea6b90a11e14d194060e17fb3
Cholewka <mdcholewka@gmail.com>
2020-07-26 14:19:42 +0200

    feat: Declare auth layout component in app's module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { ContentLayoutComponent } from './layout/content/content.component';
+import { AuthLayoutComponent } from './layout/auth/auth.component';
 import { NavigationBarComponent } from './layout/navigation-bar/navigation-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, ContentLayoutComponent, NavigationBarComponent],
+  declarations: [
+    AppComponent,
+    ContentLayoutComponent,
+    AuthLayoutComponent,
+    NavigationBarComponent,
+  ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
